521b8d234e3057fbeb9a05bbce0e9cbd68e53608
ytprivate <62745741+ytprivate@users.noreply.github.com>
2020-11-28 21:13:34 +0300

    Add files via upload
    Remake

---
+++ src/main/java/com/mycompany/collections/Motorcyclist.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+public class Motorcyclist {
+
+    private final List<MotorcyclistEquipment> motoEquipment;
+
+    public Motorcyclist(List<MotorcyclistEquipment> motoEquipment) {
+        this.motoEquipment = motoEquipment;
+    }
+
+
+    public List<MotorcyclistEquipment> getMotorcyclistEquipment() {
+        List<MotorcyclistEquipment> mte = this.motoEquipment;
+        List<MotorcyclistEquipment> motoEquipment = new ArrayList<>();
+        for (MotorcyclistEquipment me : mte) {
+            if (me != null) {
+                motoEquipment.add(me);
+            }
+        }
+        return motoEquipment;
+    }
+
+    public Motorcyclist sortByWeight() {
+        motoEquipment.sort(Comparator.comparing(MotorcyclistEquipment::getWeight));
+        return this;
+    }
+
+    public double getSum() {
+        double currentSum = 0.0;
+        for (MotorcyclistEquipment me : motoEquipment) {
+            currentSum += me.getPrice();
+        }
+        return currentSum;
+    }
+}
+
